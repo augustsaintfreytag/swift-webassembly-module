3e4ec91f11280826a4a78cc7c8fbb4c252905cc9
August Saint Freytag <night@apricummedia.com>
2022-01-21 15:48:25 +0100

    Add missing type export for memory address alias

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { WebAssemblyModule } from "./module"
 export { fetchWebAssemblyModuleData } from "./module-fetch"
 export { instantiateWebAssemblyModule } from "./module-init"
 export type { ModuleData } from "./module-fetch"
+export type { MemoryAddress } from "./memory-access"
 
 export default WebAssemblyModule

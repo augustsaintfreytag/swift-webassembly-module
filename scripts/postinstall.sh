e9c8d316d185d1f3bff14dee6f8d82e7111ba0b9
August Saint Freytag <night@apricummedia.com>
2022-04-08 14:03:08 +0200

    Use local marker file (untracked) to determine post-install state

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,12 +1,10 @@
 #! /usr/bin/env sh
 
 SCRIPT_DIR=$(dirname "$0")
-PROJECT_DIR=$(realpath "$SCRIPT_DIR/..")
-PARENT_DIR=$(realpath "$PROJECT_DIR/.." | xargs -0 basename)
 
-if [ "$PARENT_DIR" != "node_modules" ]
+if [ -f ".development" ]
 then
-	echo "Skipping deferred build, not in 'node_modules' directory (parent is '$PARENT_DIR')."
+	echo "Skipping deferred build, directory was not installed as a module."
 	exit 0
 fi
 
